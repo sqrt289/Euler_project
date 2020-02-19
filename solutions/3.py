3ce51ec53745913120e349651b195a9186fe60aa
sqrt289 <7u.bayramov7@gmail.com>
2020-02-19 13:56:41 +0300

    Commit

---
+++ solutions/3.py
@@ -0,0 +1,15 @@
+import math
+
+
+def issimple(a):
+    r = math.ceil(math.sqrt(a))
+    lst = []
+    for i in range(3, r):
+        if a % i == 0:
+            if not issimple(i):
+                lst.append(i)
+    return lst
+
+
+r = issimple(600851475143)
+print(max(r))
